02a8807c04d9295f61c04d06e6b07ccf2c3557c9
João Paulo Marquesini <joao.marquesini@estrategiaconcursos.com.br>
2022-03-09 19:03:55 -0300

    fixing generator scripts

--- .scripts/cache-cleaner/index.js
+++ .scripts/cache-cleaner/index.js
@@ -1,17 +1,27 @@
 import fs from 'fs-extra';
 import axios from 'axios';
 
-async function run() {
-    let files = await fs.readdir('./');
-    files = files.filter((file) => file.indexOf('.json') > -1 && file.indexOf('package.json') < 0);
-    for (let file of files) {
-        await clean(`https://purge.jsdelivr.net/gh/LittleLightForDestiny/littlelight_wishlists/${file}`);
-    }
-}
-
 async function clean(url) {
     const result = await axios.get(url);
     console.log(result.data);
+}
+
+async function cleanDir(path){
+    let all = await fs.readdir(path, {withFileTypes:true});
+    const files = all.map((f)=>f.name).filter((file) => file.indexOf('.json') > -1);
+    const dirs = all.filter((f)=>f.isDirectory()).map((f)=>f.name);
+    
+    for (let file of files) {
+        await clean(`https://purge.jsdelivr.net/gh/LittleLightForDestiny/littlelight_wishlists/${path}/${file}`);
+    }
+
+    for(let dir of dirs){
+        await cleanDir(`${path}/${dir}`);
+    }
+}
+
+async function run() {
+    await cleanDir('deliverables');
 }
 
 run();
